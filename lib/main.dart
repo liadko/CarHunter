c81fc4647c3d8bc3f78d5551c1d87be07a66b5cf
liadko <liadkoren@gmail.com>
2023-07-01 20:40:46 +0300

    Added Mystery Cards

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ import 'screens/second_screen.dart';
 
 void main() async {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: accentColor)),
       home: MainScreen(),
     ),
   );
@@ -29,15 +33,15 @@ class MainScreen extends StatelessWidget {
         title: const Text("Car Hunter"),
       ),
 
-      //body: gallery(),
+      body: gallery(),
 
-      body: Align(
-        alignment: Alignment.center,
-        child: ElevatedButton(
-          onPressed: () => moveToSecondScreen(context),
-          child: const Text("First Screen (boring)"),
-        ),
-      ),
+      // body: Align(
+      //   alignment: Alignment.center,
+      //   child: ElevatedButton(
+      //     onPressed: () => moveToSecondScreen(context),
+      //     child: const Text("First Screen (boring)"),
+      //   ),
+      // ),
     );
   }
 
@@ -62,48 +66,139 @@ class MainScreen extends StatelessWidget {
   }
 
   Widget gallery() {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 4,
-        mainAxisSpacing: 11,
-      ),
+    return ListView.separated(
       padding: const EdgeInsets.only(top: 12, left: 10, right: 10, bottom: 10),
-      itemCount: 102,
+      itemCount: 31,
+      separatorBuilder: (context, index) => const SizedBox(
+        height: 15,
+      ),
       itemBuilder: (BuildContext context, int index) {
-        return galleryItem(index);
+        return getCard(index);
       },
     );
   }
 
-  Widget galleryItem(int index) {
-    return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 20, 20, 26),
-          border: Border.all(
-            color: textColor,
-            width: 3,
-          ),
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Text(
-              index.toString().padLeft(3, '0'),
-              style: const TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.normal,
-                letterSpacing: 6,
-                color: textColor,
+  Widget getCard(int index) {
+    //return the card with the image or a mystery card
+    return getMysteryCard(index);
+  }
+
+  Widget getCardFromInternet(int a) {
+    return Container(
+      //margin: EdgeInsets.all(8.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        child: InkWell(
+          onTap: () => print("ciao"),
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(8.0),
+                  bottomRight: Radius.circular(8.0),
+                ),
+                child: Image.asset('images/mystery_bg.png',
+                    width: 300, height: 150, fit: BoxFit.fill),
               ),
-            ),
+              ListTile(
+                title: Text('Pub 1'),
+                subtitle: Text('Location 1'),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
+
+  Widget getMysteryCard(int index) {
+    return Container(
+      height: 136,
+
+      // decoration: BoxDecoration(
+      //   border: Border.all(
+      //     color: notchColor,
+      //     width: 2,
+      //   ),
+      //   borderRadius: BorderRadius.circular(13),
+      // ),
+      child: Stack(
+        children: [
+          Align(
+            alignment: const Alignment(0.0, -1.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(13)),
+              child: Image.asset('images/CarHunterCard.jpg',
+                  height: 109, width: 400, fit: BoxFit.fitWidth),
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0.0, 1),
+            child: getMysteryNotch(index),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getMysteryNotch(int index) {
+    return Container(
+      height: 41,
+      decoration: BoxDecoration(
+          //color: Color.fromARGB(255, 20, 20, 26),
+          // border: Border.all(
+          //   color: Colors.white,
+          //   width: 1,
+          // ),
+          //borderRadius: BorderRadius.circular(5),
+          ),
+      child: Stack(
+        children: [
+          //bottom part of notch
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 27,
+              decoration: const BoxDecoration(
+                color: notchColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(13),
+                  bottomRight: Radius.circular(13),
+                ),
+              ),
+            ),
+          ),
+          // notch part of notch
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: 90,
+              height: 29,
+              decoration: BoxDecoration(
+                color: notchColor,
+                borderRadius: BorderRadius.circular(13),
+              ),
+            ),
+          ),
+          //plate number
+          Align(
+            alignment: Alignment(0, 0.35),
+            child: Text(
+              index.toString().padLeft(3, '0'),
+              style: TextStyle(
+                color: textColor,
+                fontSize: 27,
+                letterSpacing: 4,
+                fontFamily: "Bahnschrift",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
+///TO ADD:
+///floating action button on each card for adding a picture to it
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color.fromARGB(255, 29, 27, 33);
-const bgColor = Color.fromARGB(255, 24, 22, 28);
-const textColor = Color.fromARGB(255, 191, 175, 212);
+const bgColor = Color(0xff18161C);
+const textColor = Color(0xFFE4E4E4);
 const appBarColor = Color.fromARGB(255, 73, 71, 102);
+const accentColor = Color.fromARGB(255, 89, 53, 107);
+const notchColor = Color(0xff272727);
